7aadf1f30789283ab1d8f0bc529f58b45c5d1392
Frankline Woma <womafamily@gmail.com>
2022-08-16 16:43:19 +0000

    my new commit

---
+++ stage.java
@@ -0,0 +1,15 @@
+1st Round
+
+1) Tell me about yourself
+2) What are the git best practices u suggested
+3) Have you done any migration from svn to github?
+4) Have you done Jenkins Migration
+5) How you creat Repository in Github
+6) How you give Acess for your users for perticular job in jenkins
+7) what are the jobs you created in jenkins
+8) Tell me what are the steps you written in ur pipeline job
+9) Maven Lifecycle
+10) how to check perticular port wether it is running on not?
+11) How you give permission to files or directories
+12) how you add  user to a group
+13) how will u check memory utilization
